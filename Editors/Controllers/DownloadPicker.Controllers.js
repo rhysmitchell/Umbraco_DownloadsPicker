30b4cd4f21100efdb35054da84f2929738092a66
Rhys.Mitchell <Rhys.Mitchell@ceredigion.gov.uk>
2017-05-19 10:38:46 +0100

    initial commit

---
+++ Editors/Controllers/DownloadPicker.Controllers.js
@@ -0,0 +1,14 @@
+﻿angular.module("umbraco").controller("DownloadPicker.Controller", function ($scope, $routeParams, dialogService) {
+    $scope.openMediaPicker = function () {
+        dialogService.mediaPicker({ callback: populateDownloads, multiPicker: true });
+    };
+
+    $scope.removeDownload = function (download) {
+        var index = $scope.control.downloads.indexOf(download);
+        $scope.control.downloads.splice(index, 1);    
+    };
+
+    function populateDownloads(data) {
+        $scope.control.downloads = data;
+    }
+});
